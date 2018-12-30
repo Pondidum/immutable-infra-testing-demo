d05beadbbc946b24cce3b6659c27008ade124665
andy <andydavies2020@gmail.com>
2018-12-30 13:24:25 +0200

    logstash: split configs into input and output

--- test/machine.test.js
+++ test/machine.test.js
@@ -33,7 +33,11 @@ describe("the machine", () => {
   it("should have the correct configuration", () => {
     const result = execute("find /etc/logstash/conf.d/* -type f");
 
-    expect(result).toEqual(["/etc/logstash/conf.d/beats.conf"]);
+    expect(result).toEqual([
+      "/etc/logstash/conf.d/beats.conf",
+      "/etc/logstash/conf.d/input.conf",
+      "/etc/logstash/conf.d/output.conf"
+    ]);
   });
 
   it("should be listening on 5044 for beats", () => testPort(5044));
